b4c7433fbba24f163b4287fcfec0759e6173e07c
Revathi <revathi9605@gmail.com>
2025-11-03 17:02:01 -0500

    Added Display function

--- indx.js
+++ indx.js
@@ -1,11 +1,15 @@
 const productNameInput = document.getElementById('product-name');
 const productPriceInput = document.getElementById('product-price');
-const productQuantity= document.getElementById('product-quantity');
+const productQuantity = document.getElementById('product-quantity');
 const addProductButton = document.getElementById('add-product');
 const cart = document.getElementById('cart');
 const totalPriceSpan = document.getElementById('total-price');
 
+let cartList = [];
 let totalPrice = 0;
+let userInputProductNme;
+let price;
+let quantity
 
 // Function to update the total price
 function updateTotalPrice(amount) {
@@ -22,7 +26,7 @@ function removeItem(event) {
 }
 
 function addProducts() {
-    let userInputProductNme = productNameInput.value.trim();
+    userInputProductNme = productNameInput.value.trim();
     let userInputProducPrice = productPriceInput.value.trim();
     let userInputQuantity = productQuantity.value.trim()
 
@@ -35,10 +39,10 @@ function addProducts() {
     } else if (userInputQuantity === "") {
         alert("Enter the Quantity")
         return
-    }   
+    }
     // create textcontent for product name and price
-    let price = parseFloat(userInputProducPrice)
-    let quantity = parseFloat(userInputQuantity)
+    price = parseFloat(userInputProducPrice)
+    quantity = parseFloat(userInputQuantity)
 
     if (isNaN(price) || price < 0) {
         alert("Enter a valid positive number for price");
@@ -49,16 +53,35 @@ function addProducts() {
         return;
     }
 
+    cartList.push({ userInputProductNme, price, quantity })
+    displayCart();
+    // //create li
+    // let li = document.createElement("li")
+    // li.textContent = `${userInputProductNme} - $${price.toFixed(2)}- Qty: ${quantity}`;
 
-    //create li
-    let li = document.createElement("li")
-    li.textContent = `${userInputProductNme} - $${price.toFixed(2)}- Qty: ${quantity}`;
-
-    //append the li to ul_Shoppinglist
-    cart.appendChild(li)
+    // //append the li to ul_Shoppinglist
+    // cart.appendChild(li)
 
     // Clear inputs
     productNameInput.value = "";
     productPriceInput.value = "";
     productQuantity.value = "";
+}
+
+function displayCart() {
+    cart.replaceChildren();
+    const fragment = document.createDocumentFragment();
+
+    if (cartList === 0) {
+        alert("Shopping cart is emplty")
+    } else {
+        for (let i = 0; i < cartList.lengtht; i++) {
+            let li = document.createElement("li")
+            li.textContent = `${userInputProductNme} - $${price.toFixed(2)}- Qty: ${quantity}`;
+
+            fragment.appendChild(li)
+
+        }
+        cart.appendChild(fragment)
+    }
 }
